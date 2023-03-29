99c734fde049d34934a47ef4f36e3b47dafd7123
YN1kiforov <yakov-nikiforov.2@mail.ru>
2023-03-29 20:03:52 +0700

    task CRUD

---
+++ controllers/taskController.js
@@ -0,0 +1,71 @@
+const Task = require('../models/task');
+
+exports.createTask = async (req, res) => {
+	try {
+		const task = new Task(req.body);
+
+		await task.save();
+
+		res.status(201).json({ success: true, task });
+	} catch (error) {
+		res.status(400).json({ success: false, error: error.message });
+	}
+};
+
+exports.getTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find();
+
+		res.status(200).json({ success: true, tasks });
+	} catch (error) {
+		res.status(400).json({ success: false, error: error.message });
+	}
+};
+
+exports.getTaskById = async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const task = await Task.findById(id);
+
+		if (!task) {
+			return res.status(404).json({ success: false, error: 'Task not found' });
+		}
+
+		res.status(200).json({ success: true, task });
+	} catch (error) {
+		res.status(400).json({ success: false, error: error.message });
+	}
+};
+
+exports.updateTaskById = async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+
+		if (!task) {
+			return res.status(404).json({ success: false, error: 'Task not found' });
+		}
+
+		res.status(200).json({ success: true, task });
+	} catch (error) {
+		res.status(400).json({ success: false, error: error.message });
+	}
+};
+
+exports.deleteTaskById = async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const task = await Task.findByIdAndDelete(id);
+
+		if (!task) {
+			return res.status(404).json({ success: false, error: 'Task not found' });
+		}
+
+		res.status(200).json({ success: true, message: 'Task deleted' });
+	} catch (error) {
+		res.status(400).json({ success: false, error: error.message });
+	}
+};
