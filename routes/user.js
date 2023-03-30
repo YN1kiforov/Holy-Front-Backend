ec7e4b06c3123f1feaa59f7a7fcc85818d17a7fc
YN1kiforov <yakov-nikiforov.2@mail.ru>
2023-03-30 19:01:13 +0700

    user routes

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/', userController.getUsers);
+
+router.post('/', userController.createUser);
+
+router.get('/:id', userController.getUserById);
+
+router.put('/:id', userController.updateUserById);
+
+router.delete('/:id', userController.deleteUserById);
+
+module.exports = router;
