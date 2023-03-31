8411a0811f15a8bf4d6b8919e9e132c6e3442c08
YN1kiforov <yakov-nikiforov.2@mail.ru>
2023-03-31 12:29:36 +0700

    connect DB and routes

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const jest = require('jest');
-
 const app = express();
+const taskRoutes = require('./routes/task.js');
+const userRoutes = require('./routes/user.js');
+require('dotenv').config();
 
-app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+	console.log('connect DB')
+);
+const db = mongoose.connection;
+
+app.use('/task', taskRoutes);
+app.use('/user', userRoutes);
+
+
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
